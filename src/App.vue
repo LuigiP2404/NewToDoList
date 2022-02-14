70c20b1fce67d0ef009c49392cb1fa5029471165
Luigi <luigipaglionicowd@gmail.com>
2022-02-14 16:39:42 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1>To-Do List</h1>
+  </header>
+  <section id="todo-list" v-cloak>
+    <h2>To-Do</h2>
+    <input type="text" v-model="newTask" @keyup.enter="addTask">
+    <button @click="addTask">Aggiungi</button>
+    <div v-if="show">
+      <todo-item v-for="(task,id) in tasks" :key="task" :todoTask="task" @removeTaskEmit="removeTask(id)">{{ id + 1 }}: {{ task }}</todo-item>
+    </div>
+    <button @click="toggleList">{{ fraseBottone }}</button>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoItem from './components/todoItem.vue'
+import "@/assets/css/styles.css";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    todoItem
+  },
+  data() {
+        return {
+            tasks: [
+              {
+                taskId: '',
+                taskText: ''
+              }
+            ],
+            newTask: '',
+            show: true
+        };
+    },
+    computed: {
+        fraseBottone() {
+            if (this.show) {
+                return 'Nascondi Lista';
+            }
+            else {
+                return 'Mostra Lista';
+            }
+        }
+    },
+    methods: {
+        addTask() {
+            let taskObj = {
+              taskId: this.tasks.length + 1,
+              taskText: this.newTask
+            }
+            this.tasks.push(taskObj);
+            window.localStorage.setItem(taskObj.taskId, taskObj.taskText);
+            this.newTask = '';
+        },
+        toggleList() {
+            this.show = !this.show;
+        },
+        removeTask(id) {
+            this.tasks.splice(id, 1);
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todo-item">
+    <p>{{ todoTask.taskId }}:</p>
+    <p>{{ todoTask.taskText }}</p>
+    <i class="fas fa-trash" style="color: crimson" @click="removeTaskEmit(todoTask.taskId)"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todoItem',
+  props: {
+    todoTask: Object,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    removeTaskEmit(id) {
+      this.$emit('removeTaskEmit', id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
